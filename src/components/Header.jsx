07a14b484a8ae808e4a42778e306bf095e933204
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-24 12:30:09 +0100

    add: header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,40 @@
+const Header = ({ currentUser }) => {
+  return (
+    <header className="header xl:w-7xl">
+      <div className="header-left">
+        <h1 className="logo">
+          <span className="hidden">NC News</span>
+          <img src="../src/assets/logo.png" alt="NC News Logo" />
+        </h1>
+      </div>
+      <nav className="header-nav">
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">Topics</a>
+          </li>
+          <li>
+            <a href="#">Articles</a>
+          </li>
+        </ul>
+      </nav>
+      <div className="header-right">
+        {currentUser && (
+          <div className="user-info">
+            {" "}
+            <span>{currentUser.name}</span>
+            <img
+              src={currentUser.avatar_url}
+              alt={currentUser.name}
+              className="user-avatar"
+            />
+          </div>
+        )}
+      </div>
+    </header>
+  );
+};
+
+export default Header;
