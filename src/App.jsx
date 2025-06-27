9960ee0cd5f5d883709d68a5d553bdf28d82ea58
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-27 12:02:12 +0100

    refactor: restructure App.jsx to use Outlet for route layout

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,37 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import ArticlesList from "./components/ArticlesList";
-import { BrowserRouter as Router, Routes, Route } from "react-router";
-import SingleArticlePage from "./components/singleArticlePage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useLocation,
+  Outlet,
+} from "react-router";
+import SingleArticlePage from "./components/SingleArticlePage";
+import TopicsNav from "./components/TopicsNav";
 import defaultAvatar from "./assets/user.jpeg";
+
+const Layout = ({ currentUser }) => {
+  const location = useLocation();
+  const showTopicsNav =
+    location.pathname === "/" || location.pathname.startsWith("/topics/");
+
+  return (
+    <div className="App">
+      <Header currentUser={currentUser} />
+      <main className="main-content container w-6xl grid grid-cols-3">
+        {showTopicsNav && (
+          <div className="topic-controller col-span-2">
+            <TopicsNav />
+          </div>
+        )}
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+};
 
 const App = () => {
   const currentUser = {
@@ -15,17 +43,16 @@ const App = () => {
 
   return (
     <Router>
-      <Header currentUser={currentUser} />
-      <main className="main-content container w-6xl grid grid-cols-3">
-        <Routes>
-          <Route path="/" element={<ArticlesList />} />
+      <Routes>
+        <Route path="/" element={<Layout currentUser={currentUser} />}>
+          <Route index element={<ArticlesList />} />
+          <Route path="topics/:topic_slug" element={<ArticlesList />} />
           <Route
-            path="/articles/:article_id"
+            path="articles/:article_id"
             element={<SingleArticlePage currentUser={currentUser} />}
           />
-        </Routes>
-      </main>
-      <Footer />
+        </Route>
+      </Routes>
     </Router>
   );
 };
