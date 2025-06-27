a62849df282b809d64c9cac2674366aa126bb1c2
Maiwand Ayubi <maiiiwand@gmail.com>
2025-06-27 15:33:08 +0100

    Add links to navbar and imported logo correctly

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,40 +1,41 @@
-const Header = ({ currentUser }) => {
-  return (
-    <header className="header container w-6xl">
-      <div className="header-left">
-        <h1 className="logo">
-          <span className="hidden">NC News</span>
-          <img src="../src/assets/logo.png" alt="NC News Logo" />
-        </h1>
-      </div>
-      <nav className="header-nav">
-        <ul>
-          <li>
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Topics</a>
-          </li>
-          <li>
-            <a href="#">Articles</a>
-          </li>
-        </ul>
-      </nav>
-      <div className="header-right">
-        {currentUser && (
-          <div className="user-info">
-            {" "}
-            <span>{currentUser.name}</span>
-            <img
-              src={currentUser.avatar_url}
-              alt={currentUser.name}
-              className="user-avatar"
-            />
-          </div>
-        )}
-      </div>
-    </header>
-  );
-};
+import React from "react";
+import { Link } from "react-router";
+import logo from "../assets/logo.png";
+
+const Header = ({ currentUser }) => (
+  <header className="header container w-6xl">
+    <div className="header-left">
+      <h1 className="logo">
+        <span className="hidden">NC News</span>
+        <img src={logo} alt="NC News Logo" />
+      </h1>
+    </div>
+    <nav className="header-nav">
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/">Topics</Link>
+        </li>
+        <li>
+          <Link to="/">Articles</Link>
+        </li>
+      </ul>
+    </nav>
+    <div className="header-right">
+      {currentUser && (
+        <div className="user-info">
+          <span>{currentUser.name}</span>
+          <img
+            src={currentUser.avatar_url}
+            alt={currentUser.name}
+            className="user-avatar"
+          />
+        </div>
+      )}
+    </div>
+  </header>
+);
 
 export default Header;
